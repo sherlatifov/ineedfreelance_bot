78ce1dc1112d5dcaeadf43c090eb377c3a5abd00
Sherz <ariansher69@gmail.com>
2026-09-01 01:47:27 +0300

    after updating models

--- database/models/__init__.py
+++ database/models/__init__.py
@@ -2,6 +2,6 @@ from .base import Base
 from .user import User
 from .freelancer import FreelancerProfile
 from .client import ClientProfile
-from .category import Category
-from .skill import Skill
-from .job import Job
+#from .category import Category
+#from .skill import Skill
+#from .job import Job
