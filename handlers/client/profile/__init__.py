cede00e44e81710af217eb5eb1975c0c5b8af035
Sherz <ariansher69@gmail.com>
2026-09-20 23:13:06 +0300

    Poshel

--- handlers/client/profile/__init__.py
+++ handlers/client/profile/__init__.py
@@ -1,8 +1,12 @@
 from aiogram import Router
 
 from .main import router as main_router
+from .navigation import router as navigation_router
+
 
 
 router = Router()
 
+
 router.include_router(main_router)
+router.include_router(navigation_router)
